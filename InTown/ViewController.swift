c59c622454b9a59f4dccc8bd7e32c3894e8cc30f
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-02 18:22:12 -0700

    Initial Commit

---
+++ InTown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InTown
+//
+//  Created by Ronald Hernandez on 9/2/15.
+//  Copyright (c) 2015 inTown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
