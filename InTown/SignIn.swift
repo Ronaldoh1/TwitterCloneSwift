62c2fcdc00e3a1b84961dee5b188827bfe6b8df9
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-02 20:01:54 -0700

    added sign in and sign up vc

---
+++ InTown/SignIn.swift
@@ -0,0 +1,22 @@
+//
+//  SignIn.swift
+//  InTown
+//
+//  Created by Ronald Hernandez on 9/2/15.
+//  Copyright (c) 2015 inTown. All rights reserved.
+//
+
+import UIKit
+
+class SignIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 1.0/255.0, alpha: 1.0)
+    }
+
+
+
+}
---
+++ InTown/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  InTown
+//
+//  Created by Ronald Hernandez on 9/2/15.
+//  Copyright (c) 2015 inTown. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+   
+}
